092c571b51de73d5017d9c60fefa5839d88d6c6d
Ricardo <Riickyluja19@gmail.com>
2022-07-17 16:42:13 -0300

    OT254-69
    OT254-69

---
+++ OT254-69/procesar_datos.py
@@ -0,0 +1,9 @@
+
+#funcion para procesar datos univ. Nacional del comahue.
+def univ_del_salvador_data_process():
+        pass
+
+#funcion para procesar datos univ. del salvador.
+def univ_del_comahue_data_process():
+        pass
+
