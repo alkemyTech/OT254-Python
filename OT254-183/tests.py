084222b9392d222746d6d272bc40ecc0a826e3db
AndresGB1 <a.galvisbolivar@gmail.com>
2022-08-12 12:45:58 -0500

    OT254-183

--- OT254-183/tests.py
+++ OT254-183/tests.py
@@ -7,29 +7,39 @@ from relation import maper as maper3, reducer_relation as reduce3, read_file as 
 
 
 class TestTop10Post(unittest.TestCase):
+    """Class to test the functions that get the top 10 posts
+
+    Args:
+        unittest (TestCase): TestCase class
+    """
     def test_file(self):
-        #Read the file
+        #Read the file verify the type of the data
         self.assertIsInstance(read_file1('posts.xml'), ET.Element)    
 
     def test_mapper(self):    
-        #Test maper
+        #Test maper verify the length of the list
         self.assertEqual(len(maper1(read_file1('posts.xml'))),49569)
 
     def test_reducer(self):
-        #Test reducer
+        #Test reducer verify the logics of the reducer
         self.assertEqual(reduce1([[1,1],[2,1],[2,1],[3,1]]), [{'Count': 2, 'ViewCount': 2},{'Count': 1, 'ViewCount': 1}, {'Count': 1, 'ViewCount': 3}])
 
 class TestTop10Time(unittest.TestCase):
+    """Class to test the functions that get the top 10 by time 
+    
+    Args:
+        unittest (TestCase): TestCase class
+    """
     def test_file(self):
-        #Read the file
+        #Read the file verify the type of the data
         self.assertIsInstance(read_file2('posts.xml'), ET.Element)    
 
     def test_mapper(self):    
-        #Test maper
+        #Test maper verify the length of the list
         self.assertEqual(len(maper2(read_file2('posts.xml'))),41224)
 
     def test_reducer(self):
-        #Test reducer
+        #Test reducer verify the logics of the reducer
         result = [[1,203],[3,90],[2,24],[4,18]]
         df = pd.DataFrame(result)
         df.columns = ['Id', 'Time']
@@ -37,17 +47,22 @@ class TestTop10Time(unittest.TestCase):
         self.assertEqual(reduce2([[1,203],[2,24],[3,90],[4,18]]).values.tolist(), df)
     
 class TestRelation(unittest.TestCase):
+    """Class to test the functions that relate by AnswerCount and Score
+
+    Args:
+        unittest (TestCase): TestCase class
+    """
     
     def test_file(self):
-        #Read the file
+        #Read the file verify the type of the data
         self.assertIsInstance(read_file3('posts.xml'), ET.Element)    
 
     def test_mapper(self):    
-        #Test maper
+        #Test maper verify the length of the list
         self.assertEqual(len(maper3(read_file3('posts.xml'))),13946)
 
     def test_reducer(self):
-        #Test reducer
+        #Test reducer verify the logics of the reducer
         self.assertEqual(reduce3([(.2, .3), (.0, .6), (.6, .0), (.2, .1)]).values.tolist(), [[1.0, 0.3], [0.3, 1.0]])
 
 if __name__ == '__main__':
